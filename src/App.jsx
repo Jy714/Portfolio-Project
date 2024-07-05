06156dbce7156235fe1e965ab231b05b98988c3b
Jy714 <yongjeremy0714@gmail.com>
2024-07-05 13:58:22 +0800

    projects-motions

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
+
 export default function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 ">
