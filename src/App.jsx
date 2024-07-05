e4eb6e89084572915ce7ff2d85e29fa162fb5650
Jy714 <yongjeremy0714@gmail.com>
2024-07-05 13:03:26 +0800

    contact-part

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
         <Technologies />
         <Experience />
         <Projects />
+        <Contact />
       </div>
     </div>
   )
